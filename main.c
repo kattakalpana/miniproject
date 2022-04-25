ced0ba9d1832a324e5a0ab043f9778c44b5353c9
kattakalpana <104248383+kattakalpana@users.noreply.github.com>
2022-04-25 18:42:07 +0530

    create main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int r,t,h,e,m,sc,so;
 printf("Enter Student's Roll no:");
 scanf("%d",&r);
 printf("Marks of student:");
-scanf("%d%d%d%d%d%d",&t,&h,&e,&m,&sc,&so);
+scanf("%d %d %d %d %d %d",&t,&h,&e,&m,&sc,&so);
 int total;
 total=t+h+e+m+sc+so;
 printf("\nTotal Marks:%d",total);
@@ -17,12 +17,12 @@ if (per>=75)
 ch='D';
 else if ((per<75)&&(per>=65))
 ch='F';
-else if ((per<60)&&((per>=50))
-ch='S'
-else if ((per<50)&&((per>=35))
-ch='T'
+else if ((per<60)&&(per>=50))
+ch='S';
+else if ((per<50)&&(per>=35))
+ch='T';
 else
-ch='f'
+ch='f';
 switch(ch)
 {
 case'D':if("Grade=Distinction");break;
